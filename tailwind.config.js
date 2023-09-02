c1fe28d505ef385afae0ed9ed3e3f42a24c4c22f
joshuatauro <joshuatauro45@gmail.com>
2023-09-02 17:57:30 +0530

    .

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js,css}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "primary": "poppins"
+      },
+      colors:{
+        "cta-fade": "#d6d7d7"
+      }
+    },
+  },
+  plugins: [],
+}
+
